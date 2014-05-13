ca6bad3b225f682828d7b5436498165fb810893d
Duncan Murray <djmurray@acutesoftware.com.au>
2014-05-14 00:42:51 +0930

    first commit

---
+++ Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim FILE_NAME As String = Environment.CurrentDirectory & "\launch.cfg"
+        Dim launchString As String = ""
+
+        If System.IO.File.Exists(FILE_NAME) = True Then
+            Dim objReader As New System.IO.StreamReader(FILE_NAME)
+            launchString = objReader.ReadLine()
+            System.Diagnostics.Process.Start(launchString)
+
+        End If
+    End Sub
+
+End Module
---
+++ README.md
@@ -0,0 +1,14 @@
+#Launch
+####...for when Windows Task Schedular just refuses to run a Python script
+<br />
+
+
+1. Using Visual Studio 2008 or later, compile Module1.vb to an exe (Launch.exe)
+<br />
+2. Edit the Launch.cfg file to call a Batch file
+    d:\py\_djm\DAILY.BAT
+<br />
+3. Edit the Batch file (e.g. DAILY.BAT) to call a Python script<br />
+    start C:\Python27\Python.exe D:\py\_djm\daily_python_tasks.py
+<br />
+<br />
